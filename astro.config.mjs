945362d5cf7e3b2ebcda718f9a4d03ca07fd3a44
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-08-30 17:50:22 -0300

    feat: actualizar sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,59 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Documentación Técnica',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Ubiufboeuf/documentacion-tecnica' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Visión General',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+						{ label: 'Introducción', slug: 'overview' },
+						{ label: 'Arquitectura', slug: 'overview/arquitectura' },
+						{ label: 'Funcionalidades', slug: 'overview/funcionalidades' }
+					]
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Manual del Sistema',
+					items: [
+						{ label: 'Frontend', slug: 'system/frontend' },
+						{ label: 'Backend', slug: 'system/backend' },
+						{ label: 'Flujo de Compra', slug: 'system/flujo-compra' },
+						{ label: 'Seguimiento en Tiempo Real', slug: 'system/seguimiento' },
+						{ label: 'Viajes Personalizados', slug: 'system/viajes-personalizados' }
+					]
 				},
+				{
+					label: 'Manual de Desarrollador',
+					items: [
+						{ label: 'Introducción', slug: 'developer' },
+						{ label: 'Frontend (Dev)', slug: 'developer/frontend-dev' },
+						{ label: 'Backend (Dev)', slug: 'developer/backend-dev' },
+						{ label: 'Base de Datos', slug: 'developer/database' }
+					]
+				},
+				{
+					label: 'Infraestructura',
+					items: [
+						{ label: 'Despliegue', slug: 'infra/deployment' },
+						{ label: 'Seguridad', slug: 'infra/seguridad' }
+					]
+				},
+				{
+					label: 'Notas de Estudio',
+					items: [
+								{ label: 'Protocolos', slug: 'study-notes/protocolos' },
+								{ label: 'Herramientas', slug: 'study-notes/herramientas' },
+								{ label: 'Links', slug: 'study-notes/links' }
+					]
+				},
+				{
+					label: 'Anexos',
+					items: [
+								{ label: 'Changelog', slug: 'annex/changelog' },
+								{ label: 'Presentaciones', slug: 'annex/canva' },
+								{ label: 'Bibliografía', slug: 'annex/bibliografia' }
+					]
+				}
 			],
 		}),
 	],
